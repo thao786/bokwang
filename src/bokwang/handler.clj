16370e386ec6613927036f5a21f4169c54c5e27f
thao ng <roseskindergarten@gmail.com>
2014-08-28 19:16:58 -0400

    hgfr

---
+++ src/bokwang/handler.clj
@@ -0,0 +1,13 @@
+;lein new compojure bokwang
+(ns bokwang.handler
+  (:require [compojure.core :refer :all]
+            [compojure.handler :as handler]
+            [compojure.route :as route]))
+
+(defroutes app-routes
+  (GET "/" [] "Hello World")
+  (route/resources "/")
+  (route/not-found "Not Found"))
+
+(def app
+  (handler/site app-routes))
