26a35928ebcd88655515a02ad79c1ee5ac4e8e18
thao ng <roseskindergarten@gmail.com>
2014-08-31 00:30:44 -0400

    gfd

--- src/bokwang/lib.clj
+++ src/bokwang/lib.clj
@@ -5,4 +5,4 @@
 
 
 (def static-file-host "http://li679-206.members.linode.com:3000/file")
-(def host "http://li679-206.members.linode.com:3000/")
+(def host "http://li679-206.members.linode.com:3000")
