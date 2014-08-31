5dbe856ece4c412a25aa74ec5e939e2322e7c8a2
thao ng <roseskindergarten@gmail.com>
2014-08-31 00:24:43 -0400

    hgf

--- src/bokwang/lib.clj
+++ src/bokwang/lib.clj
@@ -1,4 +1,8 @@
 (ns bokwang.lib)
 
-(def static-file-host "http://localhost:3000/file")
-(def host "http://localhost:3000")
+;(def static-file-host "http://localhost:3000/file")
+;(def host "http://localhost:3000")
+
+
+(def static-file-host "http://li679-206.members.linode.com/file")
+(def host "http://li679-206.members.linode.com/")
