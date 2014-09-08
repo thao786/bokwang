ae7a0b5e76bcedc758e30987ce9d44ee2c3e540c
thao ng <roseskindergarten@gmail.com>
2014-09-08 13:26:41 -0400

    Gfd

--- src/bokwang/lib.clj
+++ src/bokwang/lib.clj
@@ -4,11 +4,11 @@
 ;(def host "http://localhost:3000")
 
 
-(def static-file-host "http://50.116.53.36:3000/file")
-(def host "http://50.116.53.36:3000")
+(def static-file-host "http://localhost:3000/file")
+(def host "http://localhost:3000")
 
-(def bootstrap-host "http://50.116.53.36:3000/bootstrap")
-(def image-host "http://50.116.53.36:3000/image")
+(def bootstrap-host "http://localhost:3000/bootstrap")
+(def image-host "http://localhost:3000/image")
 
 ;(def static-file-host "http://localhost:3000/file")
 ;(def host "http://localhost:3000")
