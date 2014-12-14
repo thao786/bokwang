67aca413013b8e079a3e6590694e3c4ef6bbc6c5
thao nguyen <thao@li777-224.members.linode.com>
2014-12-14 18:52:30 +0000

    GFd

--- src/bokwang/lib.clj
+++ src/bokwang/lib.clj
@@ -4,11 +4,11 @@
 ;(def host "http://localhost:3000")
 
 
-(def static-file-host "http://localhost:3000/file")
-(def host "http://localhost:3000")
+(def static-file-host "http://lotus-zen.com/file")
+(def host "http://lotus-zen.com")
 
-(def bootstrap-host "http://localhost:3000/bootstrap")
-(def image-host "http://localhost:3000/image")
+(def bootstrap-host "http://lotus-zen.com/bootstrap")
+(def image-host "http://lotus-zen.com/image")
 
 ;(def static-file-host "http://localhost:3000/file")
 ;(def host "http://localhost:3000")
